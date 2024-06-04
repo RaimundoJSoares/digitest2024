e5896cabd11d4b35709720eb4ba248979ed43556
Raimundo <raimundojssoares@gmail.com>
2024-06-04 17:48:39 -0300

    fix

--- myinterview/src/main/java/com/example/RESTAPIClientTask.java
+++ myinterview/src/main/java/com/example/RESTAPIClientTask.java
@@ -1,19 +1,76 @@
 package com.example;
 
-/**
- * This task expects you to create an implementation of a Rest API client.
- * Your code should call the API endpoint related below.
- * After receiving the JSON Response, print out how many records exists for each gender
- * API endpoint => https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda 
- * 
- * >>> Bonus <<<
- * Generate a CSV file containing many records exists for each gender and save this file to AWS S3 Bucket
- * The filename need to contains your entire name, separated by uderscore. Example: john_lennon.csv
- * AWS S3 bucket name => interview-digiage
- * The credentials you can find in Coodesh platform or ask via e-mail for recrutamento@digiage.com.br
- */
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class RESTAPIClientTask {
 
-    // API endpoint => https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // API endpoint
+        String apiUrl = "https://3ospphrepc.execute-api.us-west-2.amazonaws.com/prod/RDSLambda";
+
+        // Create an HTTP client
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Create a GET request
+        HttpRequest request = HttpRequest.newBuilder()
+               .uri(URI.create(apiUrl))
+               .GET()
+               .build();
+
+        // Send the request and get the response
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Parse the JSON response
+        JSONObject jsonObject = new JSONObject(response.body());
+        JSONArray records = jsonObject.getJSONArray("records");
+
+        // Count the number of records for each gender
+        Map<String, Integer> genderCounts = new HashMap<>();
+        for (int i = 0; i < records.length(); i++) {
+            JSONObject record = records.getJSONObject(i);
+            String gender = record.getString("gender");
+            genderCounts.put(gender, genderCounts.getOrDefault(gender, 0) + 1);
+        }
+
+        // Print the counts
+        for (Map.Entry<String, Integer> entry : genderCounts.entrySet()) {
+            System.out.println("Gender: " + entry.getKey() + ", Count: " + entry.getValue());
+        }
+
+        // Bonus: generate a CSV file and upload it to AWS S3
+        String filename = "john_doe.csv"; // replace with your entire name, separated by underscores
+        String bucketName = "interview-digiage";
+
+        // Create a CSV file
+        StringBuilder csvContent = new StringBuilder();
+        csvContent.append("Gender,Count\n");
+        for (Map.Entry<String, Integer> entry : genderCounts.entrySet()) {
+            csvContent.append(entry.getKey()).append(",").append(entry.getValue()).append("\n");
+        }
+
+        // Upload the CSV file to AWS S3
+        S3Client s3Client = S3Client.create();
+        PutObjectRequest requestS3 = PutObjectRequest.builder()
+               .bucket(bucketName)
+               .key(filename)
+               .contentType("text/csv")
+               .build();
+
+        s3Client.putObject(requestS3, RequestBody.fromString(csvContent.toString()));
+        System.out.println("CSV file uploaded to AWS S3: " + filename);
+    }
 }
--- myinterview/src/main/java/com/example/RESTAPIServerTask.java
+++ myinterview/src/main/java/com/example/RESTAPIServerTask.java
@@ -1,14 +1,81 @@
 package com.example;
 
-/**
- * This task expects you to create an implementation of a REST API Server.
- * Your code should expose a REST API. 
- * Feel free to explore possibilities/functionalities/capabilities following Rest standard (best practices) 
- * and any framework or library to help you in this journey.
- * We suggest that your implementation have at least a CRUD scenario about any subject.
- * Be creative!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+@SpringBootApplication
+@RestController
 public class RESTAPIServerTask {
 
+    private S3Client s3Client;
+
+    public RESTAPIServerTask() {
+        // Create AWS credentials
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create("eusouRAIMUNDOJUNIOR", "UIBDIUBSADBASBDASBFBAFBAFBAS");
+
+        // Create static credentials provider
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(awsCreds);
+
+        // Create S3 client
+        s3Client = S3Client.builder()
+                .credentialsProvider(credentialsProvider)
+                .build();
+    }
+
+    @GetMapping("/buckets")
+    public String listBuckets() {
+        // List all buckets in S3
+        return s3Client.listBuckets().toString();
+    }
+
+    @PostMapping("/buckets/{bucketName}")
+    public String createBucket(@PathVariable String bucketName) {
+        // Create a new bucket in S3
+        s3Client.createBucket(bucketName);
+        return "Bucket created successfully";
+    }
+
+    @PutMapping("/buckets/{bucketName}/objects/{objectKey}")
+    public String uploadObject(@PathVariable String bucketName, @PathVariable String objectKey, @RequestBody String objectContent) {
+        // Upload an object to S3
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .contentType("text/plain")
+                .build();
+
+        s3Client.putObject(request, objectContent);
+        return "Object uploaded successfully";
+    }
+
+    @GetMapping("/buckets/{bucketName}/objects/{objectKey}")
+    public String getObject(@PathVariable String bucketName, @PathVariable String objectKey) {
+        // Get an object from S3
+        S3Object object = s3Client.getObject(bucketName, objectKey);
+        return object.toString();
+    }
+
+    @DeleteMapping("/buckets/{bucketName}/objects/{objectKey}")
+    public String deleteObject(@PathVariable String bucketName, @PathVariable String objectKey) {
+        // Delete an object from S3
+        s3Client.deleteObject(bucketName, objectKey);
+        return "Object deleted successfully";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RESTAPIServerTask.class, args);
+    }
 }
